bf24a029f2b8863e458424b159369c2a862b6f05
HiqueRS <riquesouzalima11@gmail.com>
2020-04-01 20:20:24 -0300

    spawnzinho bobo

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -7,16 +7,43 @@ public class Spawn : MonoBehaviour
 
     private Transform minArea;
     private Transform maxArea;
+    public GameObject ant;
+    private float time;
+    private float waitTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 0;
+        waitTime = 0;
+
+        minArea = transform.GetChild(0);
+        maxArea = transform.GetChild(1);
+
+       
+
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Spanw();
     }
+
+    void Spanw()
+    {
+        time += Time.deltaTime;
+        if (time > waitTime)
+        {
+
+            Vector3 pos = new Vector3(Random.Range(minArea.position.x, maxArea.position.x), 0, Random.Range(minArea.transform.position.z, maxArea.transform.position.z));
+
+            GameObject.Instantiate(ant, pos, Quaternion.identity);
+
+            waitTime = Random.Range(10.0f, 15.0f);
+            time = 0;
+        }
+    }
+
 }
