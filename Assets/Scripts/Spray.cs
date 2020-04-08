0072be68b38719a9d2405f6fede5061d8ed99759
Henrique Souza de Lima <riquesouzalima11@gmail.com>
2020-04-08 17:35:17 -0300

    Spray rotation

---
+++ Assets/Scripts/Spray.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spray : MonoBehaviour
+{
+    
+    private Transform target;
+    public float range;
+    private Transform partToRotate;
+    public float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+        partToRotate = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation,Time.deltaTime *turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0.0f, rotation.y, 0.0f);
+
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Ant");
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearstEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearstEnemy = enemy;
+            }
+        }
+
+        if(nearstEnemy != null && shortestDistance <= range)
+        {
+            target = nearstEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
